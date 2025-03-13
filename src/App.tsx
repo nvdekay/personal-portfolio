90a75368c0a8cc5fb685a4e75e707563634a8de6
nvdekay <khanhnvd.work@gmail.com>
2025-03-14 02:45:40 +0700

    added experiences

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Body from "./components/Body";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Body />
         <About />
         <Technologies />
+        <Experience />
       </div>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"> 
       </div>
