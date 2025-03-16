299d0a7e8d5db3c6f9e5d78d189299038b3be7fb
nvdekay <khanhnvd.work@gmail.com>
2025-03-16 23:12:00 +0700

    add contact section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Technologies />
         <Experience />
         <Projects />
+        <Contact />
       </div>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"> 
       </div>
