b0325f523aed5592c59394db52562c2db612eae2
nvdekay <khanhnvd.work@gmail.com>
2025-03-16 22:52:39 +0700

    update content CONTACT

--- src/constants/index.js
+++ src/constants/index.js
@@ -32,7 +32,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Cau Giay, Ha Noi, Viet Nam",
+  phoneNo: "+84 123 456 789",
+  email: "khanhnvd.work@gmail.com",
 };
