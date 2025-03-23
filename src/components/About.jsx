afd4220e797373063cd9a6d9b8eeaa0f5582c755
nvdekay <khanhnvd.work@gmail.com>
2025-03-23 18:23:15 +0700

    update: add motion for about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,42 @@
 
 import aboutImg from "../assets/images/avatars/about.png";
 import { ABOUT_TEXT } from "../constants";
+import { motion } from "framer-motion";
 
 const About = () => {
     return (
-        <div className="border-b border-neutral-900 pb-4">
-            <h1 className="my-20 text-center text-4xl font-bold">
-                About
-                <span className="text-primary"> me</span>
-            </h1>
-            <div className="flex flex-wrap">
-                <div className="w-full lg:w-1/2 lg:p-8">
-                    <div className="flex items-center justify-center">
-                        <img src={aboutImg} alt="about" className="w-80"/>
-                    </div>
-                </div>
+        <div className="my-20 ml-80 mt-72 mb-96 border-b border-neutral-900">
 
-                <div className="w-full lg:w-1/2">
+            <h1 className="my-20 text-center text-4xl font-bold">
+                About <span className="text-primary">me</span>
+            </h1>
+
+            <div className="flex flex-wrap">
+
+                <motion.div
+                    initial={{ x: -100, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    className="w-full lg:w-1/2 lg:p-8"
+                >
+                    <div className="flex items-center justify-center">
+                        <img src={aboutImg} alt="about" className="w-80" />
+                    </div>
+                </motion.div>
+
+                <motion.div
+                    initial={{ x: 100, opacity: 0 }}
+                    whileInView={{ x: 0, opacity: 1 }}
+                    transition={{ duration: 1 }}
+                    className="w-full lg:w-1/2"
+                >
                     <div className="flex justify-center lg:justify-start">
                         <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
                     </div>
-                </div>
+                </motion.div>
             </div>
-
         </div>
-    )
+    );
 };
 
 export default About;
