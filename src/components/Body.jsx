15309663631b7fc90af4ff9e8635dfeb47b06f63
nvdekay <khanhnvd.work@gmail.com>
2025-03-23 19:17:05 +0700

    update: add content in body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,14 +2,16 @@ import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/images/avatars/deekayProfile.png";
 const Body = () => {
     return (
-        <div className="my-20 ml-80 mt-72 mb-96 border-b border-neutral-900 pb-4 lg:mb-40">
+        <div className="my-20 ml-80 mt-72 mb-96 pb-4 lg:mb-40">
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="pb-16 text-4xl font-thin tracking-tight lg:mt-16
-                        lg:text-4xl">Nguyen Vu Dang Khanh</h1>
+                        <h1 className="text-4xl font-thin tracking-tight 
+                        lg:text-3xl">Hello, I am</h1>
+                        <h1 className="text-indigo-400 pb-8 text-4xl font-bold tracking-tight lg:mt-4
+                        lg:text-5xl">Nguyen Vu Dang Khanh</h1>
                         <span className="bg-gradient-to-r from-pink-300 to-purple-500
-                        via-slate-500 bg-clip-text text-2xl tracking-tight text-transparent">
+                        via-slate-500 bg-clip-text lg:text-3xl tracking-tight text-transparent">
                             I'm a <span>Software Engineer</span>
                         </span>
                         <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
