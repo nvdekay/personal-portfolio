76fd2c0a2174a18dd220ae184bf92dc8ed0e0cfc
nvdekay <khanhnvd.work@gmail.com>
2025-03-25 23:36:04 +0700

    add motion for body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,22 +1,60 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/images/avatars/deekayProfile.png";
+import { motion } from "framer-motion";
+
+const container = (delay) => ({
+    hidden: {
+        opacity: 0,
+        x: -100,
+    },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            duration: 1,
+            delay,
+        }
+    }
+
+})
+
 const Body = () => {
     return (
-        <div className="my-20 ml-80 mt-72 mb-96 pb-4 lg:mb-40">
+        <div className="my-20 ml-80 mt-72 mb-96 pb-6 lg:mb-40">
             <div className="flex flex-wrap">
                 <div className="w-full lg:w-1/2">
                     <div className="flex flex-col items-center lg:items-start">
-                        <h1 className="text-4xl font-thin tracking-tight 
-                        lg:text-3xl">Hello, I am</h1>
-                        <h1 className="text-indigo-400 pb-8 text-4xl font-bold tracking-tight lg:mt-4
-                        lg:text-5xl">Nguyen Vu Dang Khanh</h1>
-                        <span className="bg-gradient-to-r from-pink-300 to-purple-500
-                        via-slate-500 bg-clip-text lg:text-3xl tracking-tight text-transparent">
+                        <motion.h1
+                            variants={container(0)}
+                            initial="hidden"
+                            animate="visible"
+                            className="text-4xl font-thin tracking-tight 
+                        lg:text-3xl pb-6">
+                            Hello, I am
+                        </motion.h1>
+                        <motion.h1
+                            variants={container(0.5)}
+                            initial="hidden"
+                            animate="visible"
+                            className="bg-gradient-to-r from-pink-300 to-purple-500
+                        via-slate-500 bg-clip-text lg:text-5xl tracking-tight text-transparent pb-6 font-bold">
+                            Nguyen Vu Dang Khanh
+                        </motion.h1>
+                        <motion.span
+                            variants={container(1)}
+                            initial="hidden"
+                            animate="visible"
+                            className="bg-gradient-to-r from-pink-300 to-purple-500
+                        via-slate-500 bg-clip-text lg:text-3xl tracking-tight text-transparent pb-6">
                             I'm a <span>Software Engineer</span>
-                        </span>
-                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                        </motion.span>
+                        <motion.p
+                            variants={container(1.5)}
+                            initial="hidden"
+                            animate="visible"
+                            className="my-2 max-w-xl font-light tracking-tighter">
                             {HERO_CONTENT}
-                        </p>
+                        </motion.p>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
