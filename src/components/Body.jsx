db0307f2f3b266bbbe82a473c38edfef73189778
nvdekay <khanhnvd.work@gmail.com>
2025-03-26 00:05:25 +0700

    delete: delete pics from assets

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/images/avatars/deekayProfile.png";
 import { motion } from "framer-motion";
+import styled from 'styled-components';
 
 const container = (delay) => ({
     hidden: {
@@ -17,6 +18,98 @@ const container = (delay) => ({
     }
 
 })
+
+const StyledWrapper = styled.div`
+  button {
+   position: relative;
+   border: none;
+   background: transparent;
+   padding: 0;
+   cursor: pointer;
+   outline-offset: 4px;
+   transition: filter 250ms;
+   user-select: none;
+   touch-action: manipulation;
+  }
+
+  .shadow {
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   border-radius: 12px;
+   background: hsl(0deg 0% 0% / 0.25);
+   will-change: transform;
+   transform: translateY(2px);
+   transition: transform
+      600ms
+      cubic-bezier(.3, .7, .4, 1);
+  }
+
+  .edge {
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+   height: 100%;
+   border-radius: 12px;
+   background: linear-gradient(
+      to left,
+      hsl(340deg 100% 16%) 0%,
+      hsl(340deg 100% 32%) 8%,
+      hsl(340deg 100% 32%) 92%,
+      hsl(340deg 100% 16%) 100%
+    );
+  }
+
+  .front {
+   display: block;
+   position: relative;
+   padding: 12px 27px;
+   border-radius: 12px;
+   font-size: 1.1rem;
+   color: white;
+   background: hsl(345deg 100% 47%);
+   will-change: transform;
+   transform: translateY(-4px);
+   transition: transform
+      600ms
+      cubic-bezier(.3, .7, .4, 1);
+  }
+
+  button:hover {
+   filter: brightness(110%);
+  }
+
+  button:hover .front {
+   transform: translateY(-6px);
+   transition: transform
+      250ms
+      cubic-bezier(.3, .7, .4, 1.5);
+  }
+
+  button:active .front {
+   transform: translateY(-2px);
+   transition: transform 34ms;
+  }
+
+  button:hover .shadow {
+   transform: translateY(4px);
+   transition: transform
+      250ms
+      cubic-bezier(.3, .7, .4, 1.5);
+  }
+
+  button:active .shadow {
+   transform: translateY(1px);
+   transition: transform 34ms;
+  }
+
+  button:focus:not(:focus-visible) {
+   outline: none;
+  }`;
+
 
 const Body = () => {
     return (
@@ -52,9 +145,18 @@ const Body = () => {
                             variants={container(1.5)}
                             initial="hidden"
                             animate="visible"
-                            className="my-2 max-w-xl font-light tracking-tighter">
+                            className="my-2 max-w-xl font-light tracking-tighter pb-6">
                             {HERO_CONTENT}
                         </motion.p>
+
+                        <StyledWrapper>
+                            <button onclick="window.location.href='contact.jsx'">
+                                <span className="shadow" />
+                                <span className="edge" />
+                                <span className="front text"> Contact me
+                                </span>
+                            </button>
+                        </StyledWrapper>
                     </div>
                 </div>
                 <div className="w-full lg:w-1/2 lg:p-8">
